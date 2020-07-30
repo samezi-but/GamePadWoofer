3b42e0b04ed7efc9b1b3a556c14c2f198f96f9a5
サメジ部長 <d_viper@hotmail.co.jp>
2020-07-30 12:51:37 +0900

    正しく周波数から低音を取り出すように修正

--- Assets/Scripts/ControllerVibration.cs
+++ Assets/Scripts/ControllerVibration.cs
@@ -8,18 +8,27 @@ public class ControllerVibration : MonoBehaviour
     [SerializeField] float left_power = 1;
     [SerializeField] float duration = 0.5f;
 
+    public AudioSource audioSource;
     public bool isRightVibration = false;
     public bool isLeftVibration = false;
     void Update()
     {
-        if (Input.GetButtonDown("Fire1") || isRightVibration)
+        if (isRightVibration)
         {
-            StartCoroutine("RightVibration");
+            GamePad.SetVibration(0, 0, right_power);
+        }
+        else
+        {
+            GamePad.SetVibration(0, 0, 0);
         }
 
-        if (Input.GetButtonDown("Fire2") || isLeftVibration)
+        if (isLeftVibration)
         {
-            StartCoroutine("LeftVibration");
+            GamePad.SetVibration(0, left_power, 0);
+        }
+        else
+        {
+            GamePad.SetVibration(0, 0, 0);
         }
     }
 
@@ -34,6 +43,11 @@ public class ControllerVibration : MonoBehaviour
     {
         GamePad.SetVibration(0, left_power, 0);
         yield return new WaitForSecondsRealtime(duration);
+        GamePad.SetVibration(0, 0, 0);
+    }
+
+    private void OnDestroy()
+    {
         GamePad.SetVibration(0, 0, 0);
     }
 
